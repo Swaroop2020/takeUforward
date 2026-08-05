3303d06c4b1d1fbe71d9ffbb7432077b4e5e09c0
swaroop2020 <swaroop8787@gmail.com>
2026-08-05 18:29:47 +0530

    Binary Search

---
+++ arrays/logic-building/src/main/java/org/example/Practice.java
@@ -0,0 +1,19 @@
+package org.example;
+
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Stack;
+
+public class Practice {
+
+    public void ops(){
+        Stack<Integer> stack = new Stack<>();
+
+        stack.push(1);
+        stack.push(2);
+        stack.push(3);
+
+
+        System.out.println(stack);
+    }
+}
